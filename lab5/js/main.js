897b37791d3733d9fa7a9f8c7ca7fb74dc4e5861
Dziuda.Alicja <215713@edu.p.lodz.pl>
2021-04-19 11:51:22 +0200

    passing keyword to function works x2

--- lab5/js/main.js
+++ lab5/js/main.js
@@ -123,23 +123,28 @@ function search(){
 	var keyword = $('#gsearch').val();
 	console.log(keyword);
 
-	var transaction = db.transaction(["customers"],"readwrite");
-	var objectStore = transaction.objectStore("customers");
-	var request = objectStore.openCursor();
+	/*var transaction = db.transaction(["customers"],"readwrite");
+	var store = transaction.objectStore("customers");
+
+	var request = store.openCursor();
 	request.onsuccess = function(event) {
 		console.log("sukces");
 		var cursor = event.target.result;
 		if (cursor) {
-			if (cursor.value.column.indexOf(keyword) !== -1) {                
+			console.log(cursor.value.name.toLowerCase());
+			if(cursor.value.name.toLowerCase().includes(filter)){
+				console.log("yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeS")
+			}		*/		
+			/*if (cursor.value.column.indexOf(keyword) !== -1) {                
 				console.log("We found a row with value: " + JSON.stringify(cursor.value));
-			}  
+			} */ 
 
-			cursor.continue();          
+			/*cursor.continue();          
 		}
-	};
+	};*/
 	
 	
-	window.location.href="index.html";
+	//window.location.href="index.html";
 
 	
 }
